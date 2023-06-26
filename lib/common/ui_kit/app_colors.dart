e2f9b640e5d411a547db237a818285f058e1ad08
danil-shtil-m10 <shtil.danil@m10.az>
2023-06-26 15:38:58 +0700

    App colors

---
+++ lib/common/ui_kit/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const buttonPrimary = Color(0xFFFE5000);
+  static const buttonSecondary = Color(0xFFF1F4F9);
+
+  static const cart = Color(0xFFFFFFFF);
+
+  static const textPrimary = Color(0xFF2B2E39);
+  static const textSecondary = Color(0xFF747B8A);
+  static const textInverted = Color(0xFFFFFFFF);
+
+  static const background = Color(0xFFFFFFFF);
+}
